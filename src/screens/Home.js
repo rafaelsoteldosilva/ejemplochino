65f9b0550e7508e80ce82ae338ea3094960c8127
Rafael Soteldo <rafael.soteldo@gmail.com>
2022-12-30 22:47:25 -0300

    done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,30 +1,38 @@
 import React, { useEffect } from "react";
 import { View, Text, Button, Image } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { getRestaurantMenu, getMenu } from "../slices/restaurantMenuSlice";
+import * as globalConstants from "../globalDefinitions/globalConstants";
+
 import store from "../appStore/store";
 
-// import { ScreenTextContext } from "../globalContexts/screenTextContext";
+import { getRestaurantMenu } from "../slices/restaurantMenuSlice";
 
 import { AppStyle } from "../globalDefinitions/globalStyles";
 
 export default function Home() {
     // const { screenText, setScreenText } = React.useContext(ScreenTextContext);
-    const dispatch = useDispatch();
     const menu = useSelector(getRestaurantMenu);
+    // const status = useSelector(getRestaurantMenuStatus);
 
-    return (
-        <View style={AppStyle.appStyle}>
-            {/* <Text style={{ color: "white" }}>Home</Text> */}
-            <Image
-                source={{ uri: menu.restaurant.facade_image_url }}
-                style={{
-                    width: 360,
-                    height: 150,
-                    marginBottom: 10,
-                    marginTop: 20,
-                }}
-            />
-        </View>
-    );
+    // If data is not loaded show Loading...
+    if (
+        store.getState().restaurantMenu.restaurantMenuStatus !==
+        globalConstants.menuReadingSucceeded
+    )
+        return <Text style={{ color: "white" }}>Loading...</Text>;
+    else
+        return (
+            <View style={AppStyle.appStyle}>
+                {/* <Text style={{ color: "white" }}>Home</Text> */}
+                <Image
+                    source={{ uri: menu.restaurant.facade_image_url }}
+                    style={{
+                        width: 360,
+                        height: 150,
+                        marginBottom: 10,
+                        marginTop: 20,
+                    }}
+                />
+            </View>
+        );
 }
--- src/screens/MenuCard.js
+++ src/screens/MenuCard.js
@@ -2,32 +2,39 @@ import React from "react";
 import { View, Text, FlatList, Button } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getRestaurantMenu } from "../slices/restaurantMenuSlice";
-import { ScreenTextContext } from "../globalContexts/screenTextContext";
+import * as globalConstants from "../globalDefinitions/globalConstants";
+
+import store from "../appStore/store";
 
 import { AppStyle } from "../globalDefinitions/globalStyles";
 import { ShowAnItem } from "../components/ShowAnItem";
+import { getRestaurantMenu } from "../slices/restaurantMenuSlice";
 
 export default function MenuCard() {
     const menu = useSelector(getRestaurantMenu);
-    const { screenText, setScreenText } = React.useContext(ScreenTextContext);
-
-    return (
-        <View style={AppStyle.appStyle}>
-            <FlatList
-                data={menu.categories}
-                keyExtractor={(item) => item.category.id}
-                renderItem={({ item, index }) => {
-                    return (
-                        <ShowAnItem
-                            itemIndex={index}
-                            itemName={item.category.name}
-                            itemImage={item.category.image_url}
-                            itemDescription={item.category.description}
-                        />
-                    );
-                }}
-            />
-        </View>
-    );
+    // const status = useSelector(getRestaurantMenuStatus);
+    if (
+        store.getState().restaurantMenu.restaurantMenuStatus !==
+        globalConstants.menuReadingSucceeded
+    )
+        return <Text style={{ color: "white" }}>Loading...</Text>;
+    else
+        return (
+            <View style={AppStyle.appStyle}>
+                <FlatList
+                    data={menu.categories}
+                    keyExtractor={(item) => item.category.id}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <ShowAnItem
+                                itemIndex={index}
+                                itemName={item.category.name}
+                                itemImage={item.category.image_url}
+                                itemDescription={item.category.description}
+                            />
+                        );
+                    }}
+                />
+            </View>
+        );
 }
